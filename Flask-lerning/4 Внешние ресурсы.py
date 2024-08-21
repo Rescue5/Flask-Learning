dcfec2efc9eb305f56ab25f9ac3eecfe455918e7
Gwyndolin <danil.zhuravlev.2015@gmail.com>
2024-08-21 16:52:44 +0300

    make commit

--- Flask-lerning/4 Внешние ресурсы.py
+++ Flask-lerning/4 Внешние ресурсы.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for, request, redirect
-
+from flask import Flask, render_template, url_for, request, redirect, flash, get_flashed_messages, session
+import time
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'askdafnpailfinealfjkafj;sdjf;afjaefjaiejjaskldasd'
 
 songs = [
     {"name": "Saw", "artist": "Orbit Culture",
@@ -23,18 +25,33 @@ def main_page():
 def register():
     if request.method == 'POST':
         if request.form['password'] != request.form['confirm_password']:
-            return render_template('main(fail_register).j2')
+            flash('Пароли должны совпадать', category='error')
         elif request.form['username'] in users_list:
-            return render_template('main(fail_register).j2')
+            flash('Вы уже зарегистрированы', category='error')
         else:
             users_list.append(request.form)
-            return redirect(url_for('main_page'))
+            flash('Вы успешно зарегистрированы!', category='succes')
     return render_template('main(register).j2')
 
 
 @app.route('/fail_register', methods=['POST', 'GET'])
 def fail_register():
     return render_template('main(fail_register).j2')
+
+
+@app.errorhandler(404)
+def main_404(error):
+    return render_template('404.html'), 404
+
+
+@app.route('/callback', methods=['POST', 'GET'])
+def main_callback():
+    if 'usercall' in session:
+        return redirect(url_for('main_page'))
+    elif request.method == 'POST' and request.form['username'] == 'admin':
+        session['usercall'] = request.form['username']
+        return redirect(url_for('register'))
+    return render_template('main(callback).j2')
 
 
 if __name__ == '__main__':
